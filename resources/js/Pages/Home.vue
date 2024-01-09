dea433c7889d6c38b3c4e61f290de55af7aa46d6
Dustin Woodard <dustin.woodard@gmail.com>
2024-01-09 13:37:53 -0700

    Add new files and update routes

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import Dropdown from '@/Components/Dropdown.vue';
+import NavLink from '@/Components/NavLink.vue';
+
+defineProps({
+  canLogin: {
+    type: Boolean,
+  },
+  canRegister: {
+    type: Boolean,
+  },
+  laravelVersion: {
+    type: String,
+    required: true,
+  },
+  phpVersion: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <div class="flex">
+      <!-- Logo -->
+      <div class="shrink-0 flex items-center">
+        <Link :href="route('dashboard')">
+        <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
+        </Link>
+      </div>
+
+      <!-- Navigation Links -->
+      <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+        <NavLink :href="route('home')" :active="route().current('home')">
+          Home
+        </NavLink>
+
+        <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+          Dashboard
+        </NavLink>
+      </div>
+    </div>
+
+
+
+    <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
+      <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="font-semibold  hover:text-gray-900 dark:text-gray-400  focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
+
+      <template v-else>
+        <Link :href="route('login')" class="font-semibold  hover:text-gray-900 dark:text-gray-400  focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</Link>
+
+        <Link v-if="canRegister" :href="route('register')" class="ms-4 font-semibold  hover:text-gray-900 dark:text-gray-400  focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</Link>
+      </template>
+    </div>
+  </div>
+</template>
+
+
+
+<style scoped></style>
